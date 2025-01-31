aa78b7efa24f15891c61702e6e3843d4adb51db2
moonl1ght <ixav1@icloud.com>
2025-01-31 12:38:51 +0300

    WIP

---
+++ macos/VAXEngine/Sources/App/MetalView.swift
@@ -0,0 +1,5 @@
+//
+// Created by Alexander Lakhonin on 03.02.2025.
+//
+
+import Foundation
---
+++ macos/VAXEngine/VAXEngine/VAXEngineApp.swift
@@ -0,0 +1,14 @@
+//
+// Created by Alexander Lakhonin on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct VAXEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
