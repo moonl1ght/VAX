caacbeb10202194179708612af143853f392f7cb
moonL1ght <ixav1@icloud.com>
2026-06-04 22:04:37 +0300

    added wheels to rover

--- src/rl/gridWorld/gwAgent.cpp
+++ src/rl/gridWorld/gwAgent.cpp
@@ -15,7 +15,7 @@ vax::objects::DrawableModel::LoadDescriptor Agent::getDrawableDescriptor() const
     vax::math::Transform transform = vax::math::Transform();
     transform.position.y = 0.31f;
     return {
-        RES_PATH("assets/models/rover_s1.glb"),
+        RES_PATH("assets/models/rover_s1_v1.glb"),
         transform,
     };
 }
